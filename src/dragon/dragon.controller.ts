ce1ecb271adc610746e86e74dfeaf3a319ebd8f5
Ahmad Siddique <asshikrani66@gmail.com>
2026-05-01 09:48:47 +0500

    feat: created controllerand service for the dragon resouce

--- src/dragon/dragon.controller.ts
+++ src/dragon/dragon.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { DragonService } from './dragon.service';
+import { CreateDragonDto } from './dto/dragon.dto';
 
 @Controller('dragon')
-export class DragonController {}
+export class DragonController {
+  constructor (private readonly dragonService: DragonService) {}
+
+  @Get() 
+  async getAllDragons() {
+    return this.dragonService.getAllDragons();
+  }
+
+  @Post('create')
+  async createDragon(@Body() createDragonDto: CreateDragonDto) {
+    return this.dragonService.createDragon(createDragonDto);
+  }
+
+  @Get(':name')
+  async getDragon(@Param('name') name: string) {
+    return this.dragonService.getDragon(name);
+  }
+}
--- src/dragon/dragon.service.ts
+++ src/dragon/dragon.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Dragon, DragonDocument } from './schema/dragon.schema';
+import { Model } from 'mongoose';
+import { CreateDragonDto } from './dto/dragon.dto';
 
 @Injectable()
-export class DragonService {}
+export class DragonService {
+  constructor(
+    @InjectModel(Dragon.name) private dragonModel: Model<DragonDocument>,
+  ) {}
+
+  async getAllDragons() {
+    return this.dragonModel.find();
+  }
+
+  async getDragon(name: string) {
+    if (!name) {
+      throw new BadRequestException({
+        status: HttpStatus.BAD_REQUEST,
+        success: false,
+        message: 'Dragon name is required',
+      });
+    }
+    const result =  this.dragonModel.findOne({ name });
+
+    if (!result) {
+      throw new NotFoundException({
+        status: HttpStatus.NOT_FOUND,
+        success: false,
+        message: 'Dragon not found',
+      });
+    }
+    return result;
+  }
+
+  async createDragon(createDragonDto: CreateDragonDto) {
+    const createdDragon = new this.dragonModel(createDragonDto);
+    return createdDragon.save();
+  }
+}
