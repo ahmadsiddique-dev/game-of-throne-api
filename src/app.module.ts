2216fb32f99b79f64e687bd47c393025b242e4de
Ahmad Siddique <asshikrani66@gmail.com>
2026-05-05 09:30:57 +0500

    "debugging": lets see hope this is gonna wokr

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharacterModule } from './character/character.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HouseModule } from './house/house.module';
 import { DragonModule } from './dragon/dragon.module';
 
 @Module({
   imports: [
-    CharacterModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(`${process.env.MONGO_URI}/got`),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: `${configService.get<string>('MONGO_URI')}/got`,
+      }),
+      inject: [ConfigService],
+    }),
+    CharacterModule,
     HouseModule,
     DragonModule,
   ],
